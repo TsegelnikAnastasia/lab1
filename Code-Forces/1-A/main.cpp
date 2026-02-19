aaea30da0e793ae9ad090209ec425d262a46c929
Anastasia <stasamar29@gmail.com>
2026-02-19 22:29:35 +0300

    Update main.cpp

--- Code-Forces/1-A/main.cpp
+++ Code-Forces/1-A/main.cpp
@@ -6,13 +6,11 @@ using namespace std;
 int main() {
 	long n, m, a, hor, ver;
 	int plits;
-	setlocale(LC_ALL, "russian");
-	cout << "Введите числа n, m, a(1<=n, m, a <=10^9)" << endl;
 	cin >> n >> m >> a;
 	hor = (n + a - 1) / a;
 	ver = (m + a - 1) / a;
 
 	plits = hor * ver;
-	cout << "Количесвто плит: " << plits;
+	cout << plits;
 	return 0;
 }
