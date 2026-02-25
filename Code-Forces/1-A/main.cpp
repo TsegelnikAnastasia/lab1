00d9f11dbd668be5f47ba9047f8e802ddae6db2a
TsegelnikAnastasia <stasamar29@gmail.com>
2026-02-25 21:13:29 +0300

    Update main.cpp

--- Code-Forces/1-A/main.cpp
+++ Code-Forces/1-A/main.cpp
@@ -2,15 +2,54 @@
 
 using namespace std;
 
-int main() {
-	long long n, m, a, hor, ver;
-	long long plits;
-	cin >> n >> m >> a;
-	hor = (n + a - 1) / a;
-	ver = (m + a - 1) / a;
+long long calculateTiles(int n, int m, int a) {
+	long long or = (n + a - 1) / a;
+	long long ver = (m + a - 1) / a;
+	return hor * ver;
+}
 
-	plits = hor * ver;
-	cout << plits << endl;
+void RunAllTsets() {
+	cout << "ЗАПУСК ТЕСТОВ" << endl;
+	int PassedTests = 0;
+	int AllTests = 0;
+
+	{
+		AllTests++;
+		long long result = calculateTiles(10, 10, 3);
+		if (result == 16) {
+			cout << "Тест 1пройден успешно" << endl;
+			PassedTests++;
+		}
+		else cout << "Тест 1 НЕ ПРОЙДЕН!" << endl;
+	}
+
+	{
+		AllTests++;
+		long long result = calculateTiles(1, 1, 100);
+		if (result == 1) {
+			cout << "Тест 2 пройжен успешно" << endl;
+			PassedTests++;
+		}
+		else cout << "Тест 2 НЕ ПРОЙДЕН! " << endl;
+	}
+}
+
+
+int main() {
+	int choice;
+	cout << "1 - запуск тестов, 2 - подсчет" << endl;
+	cin >> choice;
+	if (choice == 1) {
+		RunAllTests();
+	}
+	else {
+		int n, m, a;
+		cout << "Введите n, m, a:" << endl;
+		cin >> n >> m >> a;
+
+		long long result = calculateTiles(n, m, a);
+		cout << "Нудно плиток      " << result << endl;
+	}
 	return 0;
 }
 
